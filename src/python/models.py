b2b8e63cea41517d3f5c93e73bc7506f4207ef2b
Andrew Stocker <sleepyams@gmail.com>
2017-08-11 23:48:21 +0000

    sql alchemy model init

---
+++ src/python/models.py
@@ -0,0 +1,14 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = sa.Column(sa.Integer, primary_key=True)
+    created = sa.Column(sa.DateTime,
+                        server_default=sa.text("now()"))
+    parent_id = sa.Column(sa.Integer,
+                        sa.ForeignKey("posts.id"),
+                        nullable=False)

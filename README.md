3c9824cab6fe130fc8ee32c56f9141653bd2e7a5
Bill Cauchois <wcauchois@gmail.com>
2017-08-10 22:41:42 -0400

    more design stuff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 Lots of React boilerplate stolen from https://github.com/workco/marvin.
 
+Frontend Stuff
+---
+
+To get set up, run:
+
+    npm install
+    npm install -g webpack
+
+Run `webpack -w` to watch for changes and auto-webpack.
+
--- src/js/pages/Index.jsx
+++ src/js/pages/Index.jsx
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+class Post extends Component {
+  render() {
+    return <div className="post">
+      <div className="post-inner">
+        <div className="title">
+          <span className="author">
+            {this.props.author}
+          </span>
+        </div>
+        <div className="content">
+          {this.props.text}
+        </div>
+      </div>
+    </div>
+  }
+}
+
 export default class Index extends Component {
   render() {
     return <div>
       This is the index page.<br />
-      <Link to="/about">About page</Link>
+      <Link to="/about">About page</Link><br />
+      <div>
+        <Post author="Andrew" text="Hi my name is andrew" />
+        <Post author="Bill" text="Bill bill bill bill bill bill" />
+      </div>
     </div>;
   }
 }

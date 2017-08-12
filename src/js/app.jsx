4dfeb2e756a4034d7ee2d1217e25945c3963390d
Bill Cauchois <wcauchois@gmail.com>
2017-08-12 01:41:56 -0400

    remove dead code

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -10,44 +10,6 @@ import '../less/styles.less';
 global.Transport = Transport;
 global.store = store;
 
-/*
-class ThingComponent extends Component {
-  render() {
-    return <div>
-      <div>hi i'm a counter, my value is: <strong>{this.props.counter}</strong></div>
-      <div>
-        <button onClick={this.props.onButtonClicked}>click me</button>
-      </div>
-    </div>;
-  }
-}
-const Thing = connect(
-  state => {
-    return {
-      counter: state.get('counter')
-    };
-  },
-  dispatch => {
-    return {
-      onButtonClicked: () => dispatch({type: TEST_ACTION})
-    }
-  }
-)(ThingComponent);
-
-class Index extends Component {
-  render() {
-    return <div>
-      <h1>
-        jesus fuck this took a while
-      </h1>
-      <div>
-        <Thing />
-      </div>
-    </div>;
-  }
-}
-*/
-
 ReactDOM.render(
   <Provider store={store}>
     <Root />

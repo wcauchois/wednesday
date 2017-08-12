106da5c8c1f87e47773e5af4f130c8ac58fc691f
Bill Cauchois <wcauchois@gmail.com>
2017-08-12 13:31:26 -0400

    fix formatting in models.py

--- src/python/models.py
+++ src/python/models.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Post(Base):
-    __tablename__ = "posts"
-    id = sa.Column(sa.Integer, primary_key=True)
-    created = sa.Column(sa.DateTime,
-                        server_default=sa.text("now()"))
-    parent_id = sa.Column(sa.Integer,
+  __tablename__ = "posts"
+  id = sa.Column(sa.Integer, primary_key=True)
+  created = sa.Column(sa.DateTime,
+                      server_default=sa.text("now()"))
+  parent_id = sa.Column(sa.Integer,
                         sa.ForeignKey("posts.id"))
-    content = sa.Column(sa.Text)
+  content = sa.Column(sa.Text)

1cb528a2b37146bbb3a90cf07a7ec13cd36bf4d3
Bill Cauchois <wcauchois@gmail.com>
2017-08-12 01:42:35 -0400

    remove dead variable

--- src/js/config/routes.jsx
+++ src/js/config/routes.jsx
@@ -3,8 +3,6 @@ import {Route, Switch} from 'react-router-dom';
 import Home from 'pages/Home';
 import About from 'pages/About';
 
-const publicPath = '/';
-
 export default () => (
   <Switch>
     <Route exact path='/' component={Home} />

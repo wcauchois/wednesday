cf83148fe2845d5c31f2302b3fb2b3f69d90b5c1
Andrew Stocker <sleepyams@gmail.com>
2017-08-15 08:12:45 +0000

    undid temp changes to model that accidentally got pushed

--- src/python/models.py
+++ src/python/models.py
@@ -8,11 +8,9 @@ class Post(Base):
   __tablename__ = "posts"
   id = sa.Column(sa.Integer, primary_key=True)
   created = sa.Column(sa.DateTime,
-                      server_default=sa.text("now()"),
-                      nullable=False)
+                      server_default=sa.text("now()"))
   parent_id = sa.Column(sa.Integer,
-                        sa.ForeignKey("posts.id"),
-                        nullable=False)
+                        sa.ForeignKey("posts.id"))
   content = sa.Column(sa.Text)
   ip_address = sa.Column(sa.String(128))
 

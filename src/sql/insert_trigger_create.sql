de910f5f4fad9e699cdfdc48d6fe20abf6585af1
Andrew Stocker <sleepyams@gmail.com>
2017-08-20 07:30:07 +0000

    minor formatting

--- src/sql/insert_trigger_create.sql
+++ src/sql/insert_trigger_create.sql
@@ -1,16 +1,16 @@
 CREATE OR REPLACE FUNCTION notify_post_inserted()
   RETURNS trigger AS $$
 DECLARE
-	cur posts%ROWTYPE;
+  cur posts%ROWTYPE;
 BEGIN
-	cur = NEW;
-	WHILE cur.id > 0 LOOP
-  	PERFORM pg_notify(
-    	CAST(('pubsub_' || cur.parent_id) AS text),
-    	row_to_json(NEW)::text);
-		SELECT * INTO cur FROM posts WHERE id = cur.parent_id;
+  cur = NEW;
+  WHILE cur.id > 0 LOOP
+    PERFORM pg_notify(
+      CAST(('pubsub_' || cur.parent_id) AS text),
+      row_to_json(NEW)::text);
+    SELECT * INTO cur FROM posts WHERE id = cur.parent_id;
   END LOOP;
-	RETURN NEW;
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 

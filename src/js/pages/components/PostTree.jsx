3baab6f66e3565896f5ab8e290abb25e735fd13f
Andrew Stocker <sleepyams@gmail.com>
2017-09-28 20:16:33 +0000

    actually sort children chronologically

--- src/js/pages/components/PostTree.jsx
+++ src/js/pages/components/PostTree.jsx
@@ -6,9 +6,12 @@ import Post from './Post';
 
 class PostTreeComponent extends Component {
   render() {
+    const sorted = this.props.root.children.sort(
+      (a, b) => a.created - b.created
+    );
     return <div className="post-node">
       <Post post={this.props.root} />
-      {this.props.root.children.map((child, id) =>
+      {sorted.map((child, id) =>
         <PostTreeComponent key={id} root={child} /> 
       )}
     </div>;
